afba4e167b33d9eead99c4000e6c919433d303af
joshua121008 <joshuajasondharamaraj@gmail.com>
2026-05-18 20:30:36 +0530

    done

--- test3.py
+++ test3.py
@@ -5,3 +5,10 @@ def age():
     c=a-b
     print(s,"your age is ",c)
 age()
+def are():
+    s=input("enter your name")
+    a=int(input("enter the length of rectangle"))
+    b=int(input("enter the breadth of rectangle"))
+    c=a*b
+    print(s,"the area of rectangle is ",c)
+are()
